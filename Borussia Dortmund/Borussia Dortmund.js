ab8d95bc8e71fcfc7c24b96ded0a9b72803f481d
DrRaZar <alex.herzog2002@gmail.com>
2023-06-16 09:46:09 +0200

    Zurück Button hinzugefügt und Maximale Höhe und Länge eingefügt

--- Borussia Dortmund/Borussia Dortmund.js
+++ Borussia Dortmund/Borussia Dortmund.js
@@ -1,8 +1,22 @@
+// script.js
+
+// Funktion zum Zurückgehen zur vorherigen Seite
+function goBack() {
+    window.history.back();
+}
+
+// Funktion zum Anzeigen/Verstecken der Essensliste
+function toggleFoodList() {
+    var foodList = document.getElementById('essenListe');
+    if (foodList.style.display === 'none') {
+        foodList.style.display = 'block';
+    } else {
+        foodList.style.display = 'none';
+    }
+}
+
+// Funktion zum Anzeigen/Verstecken der Getränkeliste
 function showList(listId) {
     var list = document.getElementById(listId);
-    if (list.classList.contains("hidden")) {
-        list.classList.remove("hidden");
-    } else {
-        list.classList.add("hidden");
-    }
+    list.classList.toggle('hidden');
 }
