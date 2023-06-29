8fc8d62c834f4c6655957cbc723e6026fae389ac
DrRaZar <alex.herzog2002@gmail.com>
2023-06-29 17:42:35 +0200

    Verbesserungen und Buttons zur Getränke und Speisen Seite hinzugefügt

--- Borussia Dortmund/Borussia Dortmund.js
+++ Borussia Dortmund/Borussia Dortmund.js
@@ -1,19 +1,5 @@
-// Funktion zum Umschalten der Speisenliste
-function toggleFoodList() {
-    var essenListe = document.getElementById("essenListe");
-    if (essenListe.style.display === "none") {
-        essenListe.style.display = "block";
-    } else {
-        essenListe.style.display = "none";
-    }
+document.getElementById("backButton").addEventListener("click", goBack);
+function goBack() {
+    window.history.back();
 }
 
-// Funktion zum Umschalten der Getränkeliste
-function toggleDrinkList() {
-    var getraenkeList = document.getElementById("getraenkeList");
-    if (getraenkeList.style.display === "none") {
-        getraenkeList.style.display = "block";
-    } else {
-        getraenkeList.style.display = "none";
-    }
-}
